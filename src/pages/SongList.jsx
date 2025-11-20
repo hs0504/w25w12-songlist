1e43b43a81e182b3a972ca9e6b7477acf8e2ef28
hyeongseo choi <bluedark0504@gmail.com>
2025-11-20 13:23:18 +0900

    🚧SongCard

---
+++ src/pages/SongList.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import SongCard from '../components/SongCard.jsx'
+
+export default function SongList({ songs }) {
+  return (
+    <div className='p-6'>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {songs.map(song => (
+          <SongCard key={song.id} song={song} />
+        ))}
+      </div>
+    </div>
+  )
+}
