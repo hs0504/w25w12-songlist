a6575c4bfbd62fd60a75efd6dea8febdd747dce1
hyeongseo choi <bluedark0504@gmail.com>
2025-11-20 14:21:52 +0900

    Backend 접근

--- src/pages/SongList.jsx
+++ src/pages/SongList.jsx
@@ -4,7 +4,7 @@ import SongCard from '../components/SongCard.jsx'
 export default function SongList({ songs }) {
   return (
     <div className='p-6'>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
         {songs.map(song => (
           <SongCard key={song.id} song={song} />
         ))}
